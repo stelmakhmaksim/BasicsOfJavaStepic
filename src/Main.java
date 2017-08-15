c621df90baa44ae1978dee279996a6982841cab6
stelmakhmaksim <stelmakh96@mail.ru>
2017-08-15 13:19:31 +0300

    Реализуйте метод, возвращающий true, если среди четырех его аргументов ровно два истинны (любые).Во всех остальных случаях метод должен возвращать false.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+//Реализуйте метод, возвращающий true, если среди четырех его аргументов ровно два истинны (любые).
+//Во всех остальных случаях метод должен возвращать false.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world 2!");
+        boolean ffff = booleanExpression(false, false, false, false);
+        boolean ffft = booleanExpression(false, false, false, true);
+        boolean fftf = booleanExpression(false, false, true, false);
+        boolean fftt = booleanExpression(false, false, true, true);
+
+        boolean ftff = booleanExpression(false, true, false, false);
+        boolean ftft = booleanExpression(false, true, false, true);
+        boolean fttf = booleanExpression(false, true, true, false);
+        boolean fttt = booleanExpression(false, true, true, true);
+
+        boolean tfff = booleanExpression(true, false, false, false);
+        boolean tfft = booleanExpression(true, false, false, true);
+        boolean tftf = booleanExpression(true, false, true, false);
+        boolean tftt = booleanExpression(true, false, true, true);
+
+        boolean ttff = booleanExpression(true, true, false, false);
+        boolean ttft = booleanExpression(true, true, false, true);
+        boolean tttf = booleanExpression(true, true, true, false);
+        boolean tttt = booleanExpression(true, true, true, true);
+
+        System.out.println("ffff " + ffff);
+        System.out.println("ffft " + ffft);
+        System.out.println("fftf " + fftf);
+        System.out.println("fftt " + fftt + " <- ");
+
+        System.out.println("ftff " + ftff);
+        System.out.println("ftft " + ftft + " <- ");
+        System.out.println("fttf " + fttf + " <- ");
+        System.out.println("fttt " + fttt);
+
+        System.out.println("tfff " + tfff);
+        System.out.println("tfft " + tfft + " <- ");
+        System.out.println("tftf " + tftf + " <- ");
+        System.out.println("tftt " + tftt);
+
+        System.out.println("ttff " + ttff + " <- ");
+        System.out.println("ttft " + ttft);
+        System.out.println("tttf " + tttf);
+        System.out.println("tttt " + tttt);
+
+    }
+
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        return ((a ^ b) && (c ^ d)) || ((a ^ c) && (b ^ d));
     }
 }
