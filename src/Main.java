c86628a8ae19dda05e1e5e38a855bff71cb46eb9
stelmakhmaksim <stelmakh96@mail.ru>
2017-08-16 13:34:05 +0300

    Number of leap year on the Gregorian calendar.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
+//В Григорианском календаре год является високосным в двух случаях: либо он кратен 4, но при этом не кратен 100, либо кратен 400.
+//Реализуйте метод, вычисляющий количество високосных лет с начала нашей эры (первого года) до заданного года включительно.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world 1!");
+        Random random = new Random();
+        System.out.println(leapYearCount(random.nextInt(2017)));
+    }
+
+    public static int leapYearCount(int year) {
+        return year - year / 4 + year / 400 - year / 100;
     }
 }
