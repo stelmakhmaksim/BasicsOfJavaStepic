36fe0652d6f80cf779324484edea7c646da84d4a
stelmakhmaksim <stelmakh96@mail.ru>
2017-08-18 10:48:30 +0300

    a+b=c;with_error_0.0001

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import java.util.Random;
-
-//В Григорианском календаре год является високосным в двух случаях: либо он кратен 4, но при этом не кратен 100, либо кратен 400.
-//Реализуйте метод, вычисляющий количество високосных лет с начала нашей эры (первого года) до заданного года включительно.
+//Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
+//Допустимая погрешность – 0.0001 (1E-4)
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(leapYearCount(random.nextInt(2017)));
+        double a = 0.1;
+        double b = 0.2;
+        double c = 0.3;
+        System.out.println(doubleExpression(a, b, c));
     }
 
-    public static int leapYearCount(int year) {
-        return year / 4 + year / 400 - year / 100;
+    public static boolean doubleExpression(double a, double b, double c) {
+        return Math.abs(a + b - c) <= 1E-4;
     }
 }
