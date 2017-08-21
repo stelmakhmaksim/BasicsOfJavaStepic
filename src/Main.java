0225bcf76ddc8f171c63257e9f1c537e36207d91
stelmakhmaksim <stelmakh96@mail.ru>
2017-08-21 15:20:25 +0300

    Done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
+//Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a.
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance after backslash symbol: ");
+        System.out.print(charExpression(sc.nextInt()));
+    }
+
+    public static char charExpression(int a) {
+        return (char) (']' - 1 + a);
     }
 }
