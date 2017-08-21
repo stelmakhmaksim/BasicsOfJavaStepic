801fdbfd0496a910b788aa46372ab30a7bf5ce2f
stelmakhmaksim <stelmakh96@mail.ru>
2017-08-21 16:47:28 +0300

    Done!

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 import java.util.*;
+//Реализуйте метод, проверяющий, является ли заданное число по абсолютной величине степенью двойки.
 
-//Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a.
+/*Решать можно разными способами:
+воспользовавшись одним удобным статическим методом из класса java.lang.Integer;
+применив пару трюков из двоичной арифметики;
+написав решение "в лоб" с циклом и условными операторами (можете вернуться к этой задаче после просмотра соответствующих уроков).
+*/
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the distance after backslash symbol: ");
-        System.out.print(charExpression(sc.nextInt()));
+        System.out.print("Enter the value which you want to check: ");
+        System.out.print(isPowerOfTwo(sc.nextInt()));
     }
 
-    public static char charExpression(int a) {
-        return (char) ('\\' + a);
+    /**
+     * Checks if given <code>value</code> is a power of two.
+     *
+     * @param value any number
+     * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
+     */
+    public static boolean isPowerOfTwo(int value) {
+        //return Integer.bitCount(Math.abs(value)) == 1;
+        value = Math.abs(value);
+        return (value & -value) == value;
     }
 }
